5162263c994f1ba7eaa5c73fab884f2c247980e9
Elias Ferreira <elias.eliasferreira@gmail.com>
2020-03-03 12:27:59 -0300

    Initial Commit

---
+++ SwiftUIWarCardGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIWarCardGame
+//
+//  Created by Elias Ferreira on 03/03/20.
+//  Copyright © 2020 Elias Ferreira. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
